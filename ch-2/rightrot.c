e92b2bdcf83a3f5051de6a76aa6c4f2ccf6994aa
agray <agray@pch.com>
2018-08-20 14:04:20 -0400

    rightrot

---
+++ ch-2/rightrot.c
@@ -0,0 +1,37 @@
+/* Write a function rightrot(x, n) that returns the value
+ * of the integer x rotate to the right by n bit positions
+ */
+
+#include <stdio.h>
+#include <math.h>
+
+unsigned rightrot(unsigned x, unsigned n);
+
+int main() {
+    printf("%u\n", rightrot(297, 3));
+
+    return 0;
+}
+
+unsigned rightrot(unsigned x, unsigned n) {
+    /* find max index of int, i.e. the left-most 1 */
+    int exponent = 0;
+    while ((int)(x / pow(2, exponent)) > 0) {
+        ++exponent;
+    }
+    --exponent;
+
+    /* Store returned value in 'rotation' */
+    unsigned rotation = x;
+
+    for (int i = 0; i < n; ++i) {
+        /* right-most bit of rotation */
+        unsigned y = rotation & 1;
+        /* rotate */
+        rotation >>= 1;
+        /* append value to the left */
+        rotation += (y * (int)pow(2, exponent));
+    }
+
+    return rotation;
+}
