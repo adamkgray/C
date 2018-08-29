d1b3d72e727256b33ad6f16aa7be5b00c38a7ceb
agray <agray@pch.com>
2018-08-29 13:04:34 -0400

    top elem, duplicate, swap, clear

--- ch-4/calculator.c
+++ ch-4/calculator.c
@@ -2,97 +2,185 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #define MAXOP 100
+#define MAXVAL 100
 #define NUMBER '0'
+#define BUFSIZE 100
 
+/* Read input */
 int getop(char []);
+
+/* Handle queue */
 void push(double);
 double pop(void);
+int queueNotEmpty(int i);
+void topElement(void);
+void duplicateTopElement(void);
+void swapTopTwoElements(void);
+void clearQueue(void);
+
+double val[MAXVAL]; /* Queue */
+int sp = 0;         /* Current queue index */
+
+/* Handle buffer */
+int getch(void);
+void ungetch(int);
+
+char buf[BUFSIZE];  /* Buffer */
+int bufp = 0;       /* Current buffer index */
 
 /* Reverse Polish Calculator */
 int main() {
     int type;
-    double op1, op2;
+    double op1, op2, poppedValue;
     char s[MAXOP];
+
+    /* Initialize queue with null values */
+    int i;
+    for (i = 0; i < MAXVAL; ++i) {
+        val[i] = '\0';
+    }
 
     while((type = getop(s)) != EOF) {
         switch (type) {
-        case NUMBER:
-            push(atof(s));
-            break;
-        case '+':
-            op2 = pop();
-            op1 = pop();
-            push(op1 + op2);
-            break;
-        case '*':
-            op2 = pop();
-            op1 = pop();
-            push(op1 * op2);
-            break;
-        case '-':
-            op2 = pop();
-            op1 = pop();
-            push(op1 - op2);
-            break;
-        case '/':
-            op2 = pop();
-            op1 = pop();
-            if (op2 != 0.0) {
-                push(op1 / op2);
-            } else {
-                printf("error: zero divisor\n");
-            }
-            break;
-        case '%':
-            op2 = pop();
-            op1 = pop();
-            if (op2 != 0.0) {
-                push((int)op1 % (int)op2);
-            } else {
-                printf("error: zero divisor\n");
-            }
-            break;
-        case '\n':
-            printf("\t%.8g\n", pop());
-            break;
-        default:
-            printf("error: unknown command %s\n", s);
-            break;
+            case NUMBER:
+                push(atof(s));
+                break;
+            case '+':
+                op2 = pop();
+                op1 = pop();
+                push(op1 + op2);
+                break;
+            case '*':
+                op2 = pop();
+                op1 = pop();
+                push(op1 * op2);
+                break;
+            case '-':
+                op2 = pop();
+                op1 = pop();
+                push(op1 - op2);
+                break;
+            case '/':
+                op2 = pop();
+                op1 = pop();
+                if (op2 != 0.0) {
+                    push(op1 / op2);
+                } else {
+                    printf("\tERROR: zero divisor\n");
+                }
+                break;
+            case '%':
+                op2 = pop();
+                op1 = pop();
+                if (op2 != 0.0) {
+                    push((int)op1 % (int)op2);
+                } else {
+                    printf("\tERROR: zero divisor\n");
+                }
+                break;
+            case 'T':
+                topElement();
+                break;
+            case 'D':
+                duplicateTopElement();
+                break;
+            case 'S':
+                swapTopTwoElements();
+                break;
+            case 'C':
+                clearQueue();
+                break;
+            case '\n':
+                poppedValue = pop();
+                if (poppedValue == '~') {
+                    printf("\tReturn: null\n");
+                } else {
+                    printf("\tReturn: %.8g\n", poppedValue);
+                }
+                break;
+            case '~':
+                /* no operation */
+                break;
+            default:
+                printf("\tERROR: unknown command %s\n", s);
+                break;
         }
     }
     return 0;
 }
 
-#define MAXVAL 100
 
-int sp = 0;
-double val[MAXVAL];
 
 /* push a value onto the queue */
 void push(double f) {
     if (sp < MAXVAL) {
-        val[sp++] = f;
+        val[sp] = f;
+        ++sp;
     } else {
-        printf("error: stack full, can't push %g\n", f);
+        printf("\tERROR: queue full, can't push %g\n", f);
     }
 }
 
 /* Pop a value from the queue */
 double pop(void) {
     if (sp > 0) {
-        return val[--sp];
+        --sp;
+        int poppedValue = val[sp];
+        val[sp] = '\0';
+        return poppedValue;
     } else {
-        printf("error: stack empty\n");
-        return 0.0;
+        printf("\tERROR: queue empty\n");
+        return '~';
     }
 }
 
-#include <ctype.h>
+/* Check if queue is NOT empty at index i */
+int queueNotEmpty(int i) {
+    return (val[i] != '\0') ? 1 : 0;
+}
 
-int getch(void);
-void ungetch(int);
+void topElement(void) {
+    double topElementOfQueue = val[sp - 1];
+    if (topElementOfQueue == '\0') {
+        printf("\tTop Element of Queue: null\n");
+    } else {
+        printf("\tTop Element of Queue: %.8g\n", topElementOfQueue);
+    }
+}
+
+void duplicateTopElement(void) {
+    if (queueNotEmpty(0)) { /* At least one elem in the queue */
+        val[sp] = val[sp - 1];
+        printf("\tDuplicated %.8g\n", val[sp]);
+        ++sp;
+    } else {
+        printf("\tERROR: failed to duplicate, queue empty\n");
+    }
+}
+
+void swapTopTwoElements(void) {
+    if (queueNotEmpty(1)) { /* At least to elems in the queue */
+        int a = val[sp - 1];
+        int b = val[sp - 2];
+
+        val[sp - 2] = a;
+        val[sp - 1] = b;
+    } else {
+        printf("\tERROR: failed to swap, queue empty\n");
+    }
+}
+
+void clearQueue(void) {
+    int i;
+    for (i = 0; i < MAXVAL; ++i) {
+        val[i] = '\0';
+    }
+    sp = 0;
+    printf("\tQueue cleared\n");
+}
 
 /* Get next operator or numeric operand */
 int getop(char s[]) {
@@ -106,6 +194,21 @@ int getop(char s[]) {
 
     /* Return any character that is not a number, negative sign or decimal point */
     if (!isdigit(c) && c != '.' && c != '-') {
+        return c;
+    }
+
+    /* Check non-digit charcters
+     * Dot and minus are sepcial cases
+     * and are handled further down.
+     */
+    if (!isdigit(c) && c != '.' && c != '-') {
+        /* At this point, c could be:
+         * (1) operand
+         * (2) side effect symbol
+         * (3) garbage
+         *
+         * In any case, main() will handle it
+         */
         return c;
     }
 
@@ -148,10 +251,6 @@ int getop(char s[]) {
     return NUMBER;
 }
 
-#define BUFSIZE 100
-
-char buf[BUFSIZE];
-int bufp = 0;
 
 /* Get a (possibly pushed back) character */
 int getch(void) {
@@ -161,7 +260,7 @@ int getch(void) {
 /* push a character back on input */
 void ungetch(int c) {
     if (bufp >= BUFSIZE) {
-        printf("ungetch: too many characters\n");
+        printf("\tungetch: too many characters\n");
     } else {
         buf[bufp++] = c;
     }
